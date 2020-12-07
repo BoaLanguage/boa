3dea50a80a0f0925b8f862cac1e6291e64346d5a
Archishman Sravankumar <archie@canary.media>
2020-12-06 19:27:10 -0800

    clean up

--- src/eval.ml
+++ src/eval.ml
@@ -13,6 +13,7 @@ exception IllegalBreak
 exception IllegalContinue
 exception IllegalReturn
 exception TypecheckerFail of error_info 
+exception Unimplemented of error_info
 
 (* A type for configurations. *)
 type configuration = 
@@ -45,33 +46,26 @@ let rec lookup s x : value =
     if x = y then u
     else (lookup t x)
 
-let rec lookup_opt s x : value =
+let rec lookup_unwrap_optional s x : value =
   match s with
   | [] ->
     raise (UnboundVariable x)
   | (y,u)::t ->
     if x = y then 
-      (match u with 
+      match u with 
       | Some v -> v
-      | _ -> failwith "TYPECHECK FAIL")
-    else (lookup_opt t x)
-
-let rec lookup_value vdict vkey : value = 
-    match vdict with
-    | [] -> failwith "No member of that name/val"
-    | (k, v)::rest -> 
-      if k = vkey then v
-      else lookup_value rest vkey
+      | _ -> raise @@ TypecheckerFail ("Uninitialized variable: " ^ x)
+    else (lookup_unwrap_optional t x)
 
 let int_of_value (v : value) : int =
   match v with
   | VInt i -> i
-  | _ ->  failwith "BIG PROBLEM"
+  | _ ->  raise @@ TypecheckerFail "Expected integer" 
 
 let bool_of_value (v : value) : bool = 
   match v with
   | VBool b -> b
-  | _ -> failwith "BIG PROBLEM"
+  | _ -> raise @@ TypecheckerFail "Expected boolean"
 
 let rec pow (a : int) (b : int) : int = 
   if (b = 0) then 1 else 
@@ -94,7 +88,7 @@ let evalb (b : binop) (l : value) (r : value) : value =
   | Leq -> VBool((int_of_value l) <= (int_of_value r))
   | Geq -> VBool((int_of_value l) >= (int_of_value r))
   | Is -> VBool((int_of_value l) = (int_of_value r))
-  | In -> failwith "unimplemented"
+  | In -> raise @@ Unimplemented "binop in"
   | Neq -> VBool((int_of_value l) <> (int_of_value r))
 
 let eval_fun (f: stmt) (e: env) : value = 
@@ -106,7 +100,7 @@ let eval_fun (f: stmt) (e: env) : value =
       let closure = VClosure(args, statement, e') in 
       e' := (v, closure)::e; closure      
     )
-  | _-> failwith "TYPECHECKERFAIL"
+  | _-> raise @@ TypecheckerFail "Not a function definition"
 
 let rec evale (e : exp) (s : env) : value =
   let evaluate_var v =
@@ -116,7 +110,7 @@ let rec evale (e : exp) (s : env) : value =
     begin
       match !vr with 
       | Some v -> v
-      | None -> raise @@ TypecheckerFail "Uninitialized"
+      | None -> raise @@ TypecheckerFail "Uninitialized mutable"
     end
     | _ -> lookup_result in 
   let evaluate_application callable_expression args = 
@@ -130,10 +124,6 @@ let rec evale (e : exp) (s : env) : value =
     | VClosure (_, _, _) -> VMethodCall (obj_val, res) (** Bind object to self *)
     | v -> deref_value v in
   let evaluate_slice_access sliceable_expression index_expression = 
-    (* let obj = evale sliceable_expression s in  *)
-    (* match obj with 
-    | VDict o -> call (lookup_value o (VString("__slice__"))) ([arg])
-    | _ -> call (evaluate_attribute_access sliceable_expression "__slice__") [arg] in *)
     let arg = evale index_expression s in
     call (evaluate_attribute_access sliceable_expression "__slice__") [arg] in
   let evaluate_unary_operator operator operand = 
@@ -164,200 +154,197 @@ let rec evale (e : exp) (s : env) : value =
   | Dict expexplist -> evaluate_dict expexplist
   | Skip -> VNone
   | Lam (v, t, e) -> VClosure ([v], Return(e), ref s)
-  | _ -> failwith "Unimplemented: Expression"
+  | _ -> raise @@ Unimplemented "Expression evaluation";
 
   
-and evals (conf:configuration) : env =
+and evals (conf:configuration) : env =  
   let sigma, current_statement, next_statement, kappa = conf in
+  let evaluate_assignment identifier expression =  
+    let new_value = evale expression sigma in 
+    let new_sigma = 
+    match List.assoc_opt identifier sigma with 
+    | Some (VRef r) -> r := Some new_value; sigma 
+    | _ -> (identifier, new_value)::sigma in 
+    evals (new_sigma, next_statement, Pass, kappa) in
+  let evaluate_attribute_assignment object_expression attribute_identifier new_value_expression =
+    let new_value = evale new_value_expression sigma in 
+    let obj = evale object_expression sigma in
+    match obj with 
+    | VPreObj(old_obj) -> 
+    begin
+      match List.assoc_opt attribute_identifier old_obj with 
+      | Some (result) -> result := new_value; 
+        evals (sigma, next_statement, Pass, kappa)
+      | _ -> raise @@ TypecheckerFail "Unbound Attribute"
+    end         
+    | VObj obj -> 
+      begin
+      match List.assoc_opt attribute_identifier obj with
+      | Some (VRef value) -> 
+        value := (Some new_value); 
+        evals (sigma, next_statement, Pass, kappa)
+      | Some _ -> raise @@ TypecheckerFail "Immutable Attribute"
+      | _ -> raise @@ TypecheckerFail "Unbound Attribute"      
+      end        
+    | _ -> raise @@ TypecheckerFail "Not an object" in
+  let evaluate_member_declaration identifier = 
+    let attrs = lookup sigma "__attrs__" in 
+    match attrs with 
+    | VList lst -> 
+      let new_sigma = update sigma "__attrs__" (VList((VString(identifier))::lst)) in 
+       evals (new_sigma, Pass, next_statement, kappa) 
+    | _ -> raise @@ TypecheckerFail "__attrs__ is not a list" in
+  let evaluate_mutable_member_declaration identifier = 
+    let mattrs = lookup sigma "__mattrs__" in  
+    match mattrs with 
+    | VList lst -> 
+      let new_sigma = update sigma "__mattrs__" (VList(((VString(identifier))::lst))) in 
+        evals (new_sigma, Pass, next_statement, kappa)  
+    | _ -> raise @@ TypecheckerFail "__mattrs__ is not a list" in
+  let evaluate_if guard statement_true statement_false = 
+    let b_result = evale guard sigma in
+    match b_result with
+    | VBool b' -> if b' 
+      then evals (sigma, statement_true, next_statement, kappa) 
+      else evals (sigma, statement_false, next_statement, kappa)
+    | _-> raise @@ TypecheckerFail "If guard must be boolean" in
+  let evaluate_while guard_expression body_statement = 
+    let guard = evale guard_expression sigma in 
+    match guard with
+    | VBool b' -> if b' 
+      then 
+        let while_statement = While(guard_expression, body_statement) in 
+        let s_continue = Block([while_statement;next_statement]) in 
+        let s_break = next_statement in
+        let s_return = 
+        match kappa with 
+        | [] -> Pass  
+        | (_,_, s)::t -> s in 
+        evals (sigma, 
+          body_statement, 
+          Block([while_statement;next_statement]), 
+          (s_break, s_continue, s_return)::kappa)
+      else evals (sigma, Pass, next_statement, kappa)
+    | _ -> raise @@ TypecheckerFail "While guard must be a boolean" in
+  let evaluate_break = 
+    match kappa with 
+    | (c_b, c_c, _)::kappa_t ->  evals (sigma, c_b, Pass, kappa_t)
+    | _ -> raise IllegalBreak in
+  let evaluate_continue =
+    match kappa with
+    | (c_b, c_c, _)::kappa_t -> evals (sigma, c_c, Pass, kappa_t)
+    | _ -> raise IllegalContinue in 
+  let evaluate_class class_identifier superclass statement = 
+    let new_sigma = ("__mattrs__", VList([]))::("__attrs__", VList([]))::sigma in
+      let obj_dict = evals (new_sigma, statement, Pass, []) in
+      let new_dict = (take (List.length obj_dict - List.length sigma)) obj_dict in
+      let clobj = VObj(new_dict) in
+      evals ((class_identifier, VRef (ref (Some(clobj))))::sigma, next_statement, Pass, kappa) in
   match current_statement, next_statement with
   | Pass, Pass -> sigma
   | Pass, c -> evals (sigma, c, Pass, kappa)
-  | Assign(v, a), c ->
-    begin
-    let n = evale a sigma in 
-    let new_sigma = 
-    (match List.assoc_opt v sigma with 
-    | Some (VRef r) -> r := Some n; sigma 
-    | _ -> (v, n)::sigma)      
-    in evals (new_sigma, c, Pass, kappa)
-    end
-  | AttrAssgn(objexp, attr, e), c ->
-    begin
-      let v = evale e sigma in 
-      let obj = evale objexp sigma in
-      (match obj with 
-      | VPreObj(old_obj) -> 
-          (match List.assoc_opt attr old_obj with 
-          | Some (result) -> 
-          begin
-            result := v;
-            evals (sigma, c, Pass, kappa)
-          end
-          | _ -> failwith "Unbound attribute")
-      | VObj obj -> 
-        begin
-        let _ = 
-          (match List.assoc_opt attr obj with
-          | Some (result) -> 
-            (match result with 
-            | VRef value -> value := (Some v)
-            | _ -> failwith "Immutable Attribute")
-          | _ -> failwith "Unbound Attribute")      
-          in evals (sigma, c, Pass, kappa)
-        end
-      
-      | _ -> print_value obj; ("TYPECHECK FAIL: " ^ attr) |> failwith)
-    end
+  | Assign(v, a), _ -> evaluate_assignment v a
+  | AttrAssgn(objexp, attr, e), c -> evaluate_attribute_assignment objexp attr e
   | Decl (_, _), c -> evals (sigma, Pass, c, kappa)
-  | MutableDecl (_, v), c -> 
-    evals ((v, VRef (ref None))::sigma, c, Pass, kappa)
-  | MemDecl(_, v), c -> 
-      let attrs = lookup sigma "__attrs__" in 
-      (match attrs with 
-      | VList lst -> let new_sigma = update sigma "__attrs__" (VList((VString(v))::lst)) in 
-        evals (new_sigma, Pass, c, kappa) 
-      | _ -> failwith "TYPECHECK FAILED: ATTRS NOT A LIST")
-  | MutableMemDecl(_, v), c -> 
-      let mattrs = lookup sigma "__mattrs__" in 
-      (match mattrs with 
-      | VList lst -> let new_sigma = update sigma "__mattrs__" (VList(((VString(v))::lst))) in 
-        evals (new_sigma, Pass, c, kappa) 
-      | _ -> failwith "TYPECHECK FAILED: MATTRS NOT A LIST")
+  | MutableDecl (_, v), c -> evals ((v, VRef (ref None))::sigma, c, Pass, kappa)
+  | MemDecl(_, v), c -> evaluate_member_declaration v       
+  | MutableMemDecl(_, v), c -> evaluate_mutable_member_declaration v
   | Block(c1::t), Pass -> evals (sigma, c1, Block(t), kappa)
-  | Block(c1::t), c3 -> evals (sigma, c1, Block(t@[c3]), kappa)
-  
+  | Block(c1::t), c3 -> evals (sigma, c1, Block(t@[c3]), kappa)  
   | Block([]), c3 -> evals (sigma, Pass, c3, kappa)
-
-  | If(b,c1,c2), c3 -> let b_result = evale b sigma in 
-    (match b_result with
-    | VBool b' -> if b' 
-      then evals (sigma, c1, c3, kappa) 
-      else evals (sigma, c2, c3, kappa)
-    | _-> failwith "If guard must be a boolean")
-
-  | While(b, c1), c2 -> 
-    let b_result = evale b sigma in 
-    begin
-    match b_result with
-    | VBool b' -> if b' 
-      then 
-        let w = While(b, c1) in 
-        let s_continue = Block([w;c2]) in 
-        let s_break = c2 in
-        let s_return = 
-          (match kappa with 
-          | [] -> Pass  
-          | (_,_, s)::t -> s) in 
-        evals (sigma, c1, Block([w;c2]), (s_break, s_continue, s_return)::kappa)
-      else evals (sigma, Pass, c2, kappa)
-    | _ -> raise @@ TypecheckerFail "While guard must be a boolean"
-    end
-  | Print(a), c -> let n = evale a sigma in 
-    Pprint.print_value n; Format.printf "%s" "\n"; evals (sigma, Pass, c, kappa)
-
-  | Break, c -> 
-  begin
-    match kappa with 
-    | (c_b, c_c, _)::kappa_t ->  evals (sigma, c_b, Pass, kappa_t)
-    | _ -> raise IllegalBreak
-  end
-  | Continue, c -> 
-  begin
-    match kappa with
-    | (c_b, c_c, _)::kappa_t -> evals (sigma, c_c, Pass, kappa_t)
-    | _ -> raise IllegalContinue
-  end
+  | If(b,c1,c2), c3 ->  evaluate_if b c1 c2   
+  | While(b, c1), c2 -> evaluate_while b c1    
+  | Print(a), c -> 
+    Pprint.print_value @@ evale a sigma; Format.printf "%s" "\n"; 
+    evals (sigma, Pass, c, kappa)
+  | Break, c -> evaluate_break
+  | Continue, c -> evaluate_continue
   | Return e, _->  ("return", evale e sigma)::sigma
   | Def (rt, name, args, body), c -> 
     evals ((name, eval_fun (Def (rt, name, args, body)) sigma)::sigma, Pass, c, kappa)
-  | Class (name, super, stmt), c ->
-    let new_sigma = ("__mattrs__", VList([]))::("__attrs__", VList([]))::sigma in
-    let obj_dict = evals (new_sigma, stmt, Pass, []) in
-    let new_dict = (take (List.length obj_dict - List.length sigma)) obj_dict in
-    let clobj = VObj(new_dict) in
-    (* let clobj = VObj((name, VRef (ref (Some(clobj))))::new_dict) in *)
-    evals ((name, VRef (ref (Some(clobj))))::sigma, c, Pass, kappa)
-  | _ -> failwith "unimplemented"
+  | Class (name, super, stmt), c -> evaluate_class name super stmt
+  | _ -> raise @@ Unimplemented "Statement"
 
-and call (vclosure : value) (args : value list) : value = 
-  let zip p a = (p, a) in
-  match vclosure with 
-  | VMethodCall (obj, closure) -> 
-    (
-      match call (closure) (obj::args) with
-      | VRef r -> 
-      begin
-        match !r with
-        | Some v -> v
-        | _ -> failwith "Method returned none"
-      end
-      | v -> v
-    )
-  | VClosure (params, body, env_ref) -> 
-  let callenv = 
-    (match args with 
-    | [] -> !env_ref
-    | _ -> 
-    (List.map2 zip params args)@(!env_ref)) in 
-    lookup (evals (callenv, body, Pass, [])) "return"
-  | VRef r -> 
-    (match !r with 
+and call (callable : value) (args : value list) : value = 
+  let zip = List.map2 (fun x y -> (x,y)) in
+  let call_method obj closure = 
+    match call (closure) (obj::args) with
+    | VRef r -> 
+    begin
+      match !r with
+      | Some v -> v
+      | _ -> raise @@ TypecheckerFail "Method returned uninitialized reference"
+    end
+    | v -> v in
+  let call_closure params body env_ref = 
+    let callenv = 
+      match args with 
+      | [] -> !env_ref
+      | _ -> (zip params args)@(!env_ref) in 
+      lookup (evals (callenv, body, Pass, [])) "return" in
+  let call_reference r = 
+    match !r with 
     | Some (VObj(cls_obj) as v) -> call v args
-    | _ -> failwith "Not good")
-  | VObj cls_obj -> 
-  begin
-    match (lookup cls_obj "__init__") with
+    | _ -> raise @@ TypecheckerFail "Callable is uninitialized" in
+  let call_init class_object = 
+    match (lookup class_object "__init__") with
     | VClosure (params, body, env) as init -> 
-    let attrs = lookup cls_obj "__attrs__" in 
-    let mattrs = lookup cls_obj "__mattrs__" in
-    let extract_string_list vlist = 
-      (match vlist with 
-      | VList (val_list) -> 
-        let extract_string vstring = 
-          (match vstring  with
-          | VString s -> s
-          | _ -> failwith "INFRASTRUCTURE FAIL") in
-        List.map extract_string val_list
-      | _ -> failwith "INFRASTRUCTURE FAILLLL") in 
-    let attrs', mattrs' = extract_string_list attrs, extract_string_list mattrs in
-    let preinit_attrs = List.map (fun x -> (x, ref VNone)) (attrs'@mattrs') in
-    let cls_ref = [("__class__", ref (VRef (ref (Some (VObj cls_obj)))))] in
-    let initial_obj_list = preinit_attrs@cls_ref in
-    let instance = VPreObj(initial_obj_list) in 
-    let new_args = (VRef (ref (Some instance)))::args in
-    (match call init new_args with 
-      | VPreObj(attrsmattrs) -> 
-        let attrs = 
-          List.filter (fun (name, vref) -> List.mem name attrs') attrsmattrs in
-        let attrs = 
-          List.map (fun (name, vref) -> (name, !vref)) attrs in
-        let mattrs = List.filter (fun (name, vref) -> List.mem name mattrs') attrsmattrs in
-        let mattrs =  List.map (fun (name, vref) -> (name, VRef (ref (Some !vref)))) mattrs in
-          VObj([("__class__", VRef(ref (Some (VObj(cls_obj)))))]@attrs@mattrs)
-    | _ -> failwith "Infra fail")
-    | _ -> failwith "Typecheck fail"
-  end 
-  
-  | _ -> failwith "TYPECHECK FAIL"
+      let attrs = lookup class_object "__attrs__" in 
+      let mattrs = lookup class_object "__mattrs__" in
+      let extract_string_list vlist = 
+        match vlist with 
+        | VList (val_list) -> 
+          let extract_string vstring = 
+            match vstring  with
+            | VString s -> s
+            | _ -> raise @@ TypecheckerFail "Atrribute names must be strings" in
+          List.map extract_string val_list
+        | _ -> raise @@ TypecheckerFail "Attributes must be a list" in 
+      let attrs', mattrs' = extract_string_list attrs, extract_string_list mattrs in
+      let preinit_attrs = List.map (fun x -> (x, ref VNone)) (attrs'@mattrs') in
+      let cls_ref = [("__class__", ref (VRef (ref (Some (VObj class_object)))))] in
+      let initial_obj_list = preinit_attrs@cls_ref in
+      let instance = VPreObj(initial_obj_list) in 
+      let new_args = (VRef (ref (Some instance)))::args in
+      begin
+        match call init new_args with 
+        | VPreObj(attrsmattrs) -> 
+          let attrs = 
+            List.filter (fun (name, vref) -> List.mem name attrs') attrsmattrs in
+          let attrs = 
+            List.map (fun (name, vref) -> (name, !vref)) attrs in
+          let mattrs = List.filter (fun (name, vref) -> List.mem name mattrs') attrsmattrs in
+          let mattrs =  List.map (fun (name, vref) -> (name, VRef (ref (Some !vref)))) mattrs in
+            VObj([("__class__", VRef(ref (Some (VObj(class_object)))))]@attrs@mattrs)
+        | _ -> raise @@ TypecheckerFail "__init__ did not return pre-object"
+      end
+    | _ -> raise @@ TypecheckerFail "__init__ must evaluate to a closure"
+  in 
+  match callable with 
+  | VMethodCall (obj, closure) -> call_method obj closure
+  | VClosure (params, body, env_ref) -> call_closure params body env_ref
+  | VRef r -> call_reference r    
+  | VObj cls_obj -> call_init cls_obj  
+  | _ -> raise @@ TypecheckerFail "Expression not callable"
 
   (*
       let new_env: env = ("class", VObj(cls_obj))::(!env) in
       call (VClosure(List.tl params, body, ref new_env)) args ) *)
   and extract_class value = 
-  match value with 
-  | VRef r -> 
-  begin
-    match !r with
-    | Some (VObj (cls_ref)) -> cls_ref
-    | _ -> failwith "not a class"
-  end
-  | VObj(cls_ref) -> cls_ref
-  | _ -> failwith "Not a class"
+    match value with 
+    | VRef r -> 
+    begin
+      match !r with
+      | Some (VObj (cls_ref)) -> cls_ref
+      | _ -> raise @@ TypecheckerFail "Not a class"
+    end
+    | VObj(cls_ref) -> cls_ref
+    | _ -> raise @@ TypecheckerFail "Not a class"
 
   and deref_value value =
-  match value with 
-  | VRef r -> 
-    (match !r with 
-    | Some v -> v
-    | None -> failwith "Fail")
-  | _ -> value
+    match value with 
+    | VRef r -> 
+      (match !r with 
+      | Some v -> v
+      | None -> raise @@ TypecheckerFail "Uninitialized reference")
+    | _ -> value
