5846a22cda266137c009fa83d5e4d44b87ff0c35
Archishman Sravankumar <archie@canary.media>
2020-12-09 20:49:33 -0800

    dedents working

--- src/main.ml
+++ src/main.ml
@@ -5,6 +5,8 @@ open Parser
 let filename = ref ""
 let nocheck = ref false
 
+exception IllegalIndentationError
+
 let options = [
   "-nocheck", Arg.Unit (fun _ -> nocheck := true), "Disable type checker"
 ]
@@ -70,24 +72,49 @@ let rec print_lexbuf l =
   | l -> (Lexer.token l |> str_of_token |> Format.printf "%s "); print_lexbuf l
 
 let indent_level = ref 0
-let dedent_stack : token list ref = ref []
-
-let token_wrapper lexbuf = 
+let dedent_stack : int list ref = ref [0]
+let temp_tokens : token list ref = ref []
+let rec token_wrapper lexbuf = 
+  let rec find_dedents (dedent_stack_copy: int list) 
+    (level: int) (acc: token list): int list * token list = 
+    match dedent_stack_copy with 
+    | [] -> raise IllegalIndentationError
+    | h :: t -> if (h = level) then (dedent_stack_copy, acc)
+      else find_dedents t level (DEDENT::acc) 
+  in 
+  if !temp_tokens <> [] then 
+    let token = List.hd !temp_tokens in 
+    temp_tokens := List.tl !temp_tokens; token 
+  else if !indent_level < 0 then EOF  else
   let token = Lexer.token lexbuf in 
-  let dedents = !dedent_stack in 
   match token with 
   | NEWLINE (i) -> 
     begin
       let level = !indent_level in
       if i > level then 
         (indent_level := i;
-        dedent_stack := !dedent_stack + 1;
+        dedent_stack := i::!dedent_stack;
         INDENT)
-      else (if i = level then 
-        NOP
-      else DEDENT)
+      else if i = level then 
+        token_wrapper lexbuf
+      else 
+        let new_dedent_stack, dedent_list = find_dedents !dedent_stack i [] in 
+        indent_level := i;
+        dedent_stack := new_dedent_stack;
+        temp_tokens := dedent_list;
+        token_wrapper lexbuf
     end
+  | EOF -> let _, dedent_list = find_dedents !dedent_stack 0 [] in 
+        indent_level := -1;
+        dedent_stack := [];
+        temp_tokens := dedent_list;
+        token_wrapper lexbuf
   | _ -> token
+
+let rec print_tokens token_fun lexbuf = 
+  let token = token_fun lexbuf in 
+  print_endline @@ str_of_token token;
+  if token = EOF then () else print_tokens token_fun lexbuf
 
 let () =
   (* (1) Parse the command-line arguments. *)
@@ -103,7 +130,7 @@ let () =
   let file = open_in (!filename) in
   let lexbuf = Lexing.from_channel file in
   let e =
-    (print_lexbuf lexbuf);
+    print_tokens token_wrapper lexbuf;
     Format.printf "\n---END_LEX--";
     try Parser.prog Lexer.token lexbuf
     with Parsing.Parse_error ->
