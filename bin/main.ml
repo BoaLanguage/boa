cf7dd76d897cdeaf903480118c725541bcf8a04d
James Meyers <jwm337@cornell.edu>
2020-12-27 15:14:14 -0500

    Removed unused variables

--- bin/main.ml
+++ bin/main.ml
@@ -14,9 +14,9 @@ let options = [
 let indent_level = ref 0
 let dedent_stack : int list ref = ref [0]
 let temp_tokens : token list ref = ref []
-let rec token_wrapper lexbuf = 
+let token_wrapper lexbuf = 
   let rec find_dedents (dedent_stack_copy: int list) 
-    (level: int) (acc: token list): int list * token list = 
+      (level: int) (acc: token list): int list * token list = 
     match dedent_stack_copy with 
     | [] -> raise IllegalIndentationError
     | h :: t -> if (h = level) then (dedent_stack_copy, acc)
@@ -26,31 +26,31 @@ let rec token_wrapper lexbuf =
     let token = List.hd !temp_tokens in 
     temp_tokens := List.tl !temp_tokens; token 
   else if !indent_level < 0 then (EOF)  else
-  let token = Lexer.token lexbuf in 
-  match token with 
-  | NEWLINE (i) -> 
-    begin
-      let level = !indent_level in
-      if i > level then 
-        (indent_level := i;
-        dedent_stack := i::!dedent_stack;
-        temp_tokens := [INDENT];
-        EOL)
-      else if i = level then 
-        EOL
-      else 
-        let new_dedent_stack, dedent_list = find_dedents !dedent_stack i [EOL] in 
-        indent_level := i;
-        dedent_stack := new_dedent_stack;
-        temp_tokens := dedent_list;
-        EOL
-    end
-  | EOF -> let _, dedent_list = find_dedents !dedent_stack 0 [EOL] in 
-        indent_level := -1;
-        dedent_stack := [];
-        temp_tokens := dedent_list;
-        EOL
-  | _ -> token
+    let token = Lexer.token lexbuf in 
+    match token with 
+    | NEWLINE (i) -> 
+      begin
+        let level = !indent_level in
+        if i > level then 
+          (indent_level := i;
+           dedent_stack := i::!dedent_stack;
+           temp_tokens := [INDENT];
+           EOL)
+        else if i = level then 
+          EOL
+        else 
+          let new_dedent_stack, dedent_list = find_dedents !dedent_stack i [EOL] in 
+          indent_level := i;
+          dedent_stack := new_dedent_stack;
+          temp_tokens := dedent_list;
+          EOL
+      end
+    | EOF -> let _, dedent_list = find_dedents !dedent_stack 0 [EOL] in 
+      indent_level := -1;
+      dedent_stack := [];
+      temp_tokens := dedent_list;
+      EOL
+    | _ -> token
 
 let () =
   (* (1) Parse the command-line arguments. *)
@@ -67,7 +67,7 @@ let () =
   let lexbuf = Lexing.from_channel file in
   let e =
     (* print_tokens token_wrapper lexbuf;
-    Format.printf "\n---END_LEX--"; *)
+       Format.printf "\n---END_LEX--"; *)
     try Parser.prog token_wrapper lexbuf
     with Parsing.Parse_error ->
       let pos = lexbuf.Lexing.lex_curr_p in
